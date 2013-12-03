b6db654b35ef54a72a4fb85226299477b7c29256
Chiwan <jp.kha@live.fr>
2013-12-03 19:09:18 +0100

    V0.5

--- src/views/etat/Find.java
+++ src/views/etat/Find.java
@@ -5,8 +5,6 @@ import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.commons.cli.CommandLine;
-
 import views.Command;
 import views.State;
 
@@ -36,9 +34,7 @@ public class Find extends State {
      *
      * @param line the line
      */
-    private void add( final CommandLine line ) {
-
-    }
+ 
 
     /*
      * (non-Javadoc)
@@ -77,57 +73,4 @@ public class Find extends State {
          */
     }
 
-    /**
-     * Sets the end.
-     *
-     * @param line the new end
-     */
-    private void setEnd( final CommandLine line ) {
-        if( line.getArgList( ).size( ) != 3 ) {
-            this.printHelp( );
-        } else {
-            final String[ ] dayMonthYear = ( ( String ) line.getArgList( ).get(
-                    1 ) ).split( "/" );
-            final String[ ] hourMinute = ( ( String ) line.getArgList( )
-                    .get( 2 ) ).split( ":" );
-            if( dayMonthYear.length != 3 || hourMinute.length != 2 ) {
-                this.printHelp( );
-            }
-
-            this.end.set( Integer.valueOf( dayMonthYear[ 2 ] ),
-                    Integer.valueOf( dayMonthYear[ 1 ] ),
-                    Integer.valueOf( dayMonthYear[ 0 ] ),
-                    Integer.valueOf( hourMinute[ 0 ] ),
-                    Integer.valueOf( hourMinute[ 1 ] ), 0 );
-
-            System.out.println( "end date set" );
-        }
-
-    }
-
-    /**
-     * Sets the start.
-     *
-     * @param line the new start
-     */
-    private void setStart( final CommandLine line ) {
-        if( line.getArgList( ).size( ) != 3 ) {
-            this.printHelp( );
-        } else {
-            final String[ ] dayMonthYear = ( ( String ) line.getArgList( ).get(
-                    1 ) ).split( "/" );
-            final String[ ] hourMinute = ( ( String ) line.getArgList( )
-                    .get( 2 ) ).split( ":" );
-            if( dayMonthYear.length != 3 || hourMinute.length != 2 ) {
-                this.printHelp( );
-            }
-            this.start.set( Integer.valueOf( dayMonthYear[ 2 ] ),
-                    Integer.valueOf( dayMonthYear[ 1 ] ),
-                    Integer.valueOf( dayMonthYear[ 0 ] ),
-                    Integer.valueOf( hourMinute[ 0 ] ),
-                    Integer.valueOf( hourMinute[ 1 ] ), 0 );
-
-            System.out.println( "Start date set" );
-        }
-    }
 }
